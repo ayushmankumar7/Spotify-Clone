ee9b451e229908c31d27b4e06aef02cea1a306f4
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-12-19 02:21:45 +0530

    UI added

---
+++ spotifyclone/UI/build/precache-manifest.424401c2211727b9a49ec3f8052872c2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081a0cd4304c01b9bd45ef9339a49fda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "361466a48aae3cb9f959",
+    "url": "/static/css/main.d67bac59.chunk.css"
+  },
+  {
+    "revision": "3c5312b2c0403a1449ea",
+    "url": "/static/js/2.be61bf41.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.be61bf41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "361466a48aae3cb9f959",
+    "url": "/static/js/main.b82c30f9.chunk.js"
+  },
+  {
+    "revision": "6ca49defa095afd3e9c1",
+    "url": "/static/js/runtime-main.f9911b94.js"
+  },
+  {
+    "revision": "6baed2bf580964bec9559ad83caee43d",
+    "url": "/static/media/CircularStd-Bold.6baed2bf.otf"
+  },
+  {
+    "revision": "6365c40aa59d462f1cc52ccce9635cb4",
+    "url": "/static/media/CircularStd-Book.6365c40a.otf"
+  },
+  {
+    "revision": "ffe682691834b7deebbf5c38dbc0b1e4",
+    "url": "/static/media/heart.ffe68269.svg"
+  },
+  {
+    "revision": "3005871a5a18de80182ce2cf5a7167bd",
+    "url": "/static/media/home.3005871a.svg"
+  },
+  {
+    "revision": "d464e65a9d45f9b6b3056b0019f63a1b",
+    "url": "/static/media/library.d464e65a.svg"
+  },
+  {
+    "revision": "ceeb8cd18b9f25fec20a04dcc8d8bfb0",
+    "url": "/static/media/note.ceeb8cd1.svg"
+  },
+  {
+    "revision": "78b8b9853183e29bd82d98aafc60340c",
+    "url": "/static/media/play.78b8b985.svg"
+  },
+  {
+    "revision": "beb633b32586c3c5614c3ec14f8b8737",
+    "url": "/static/media/play.beb633b3.svg"
+  },
+  {
+    "revision": "0f6375428d8f094acc24d7e67c4ebcc1",
+    "url": "/static/media/search.0f637542.svg"
+  }
+]);
